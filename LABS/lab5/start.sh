cb7632b0a1d9b4b3aa4651428f220fd46e57f9e2
DoAlvaro <alvaro.dobro@mail.ru>
2026-03-16 14:25:33 +0300

    lab4?

--- LABS/lab5/start.sh
+++ LABS/lab5/start.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-# Пример: goalie / attacker / defender
-python3 src/main.py --team teamA --x -15 --y 0 --role attacker &
+
+python3 src/main.py --team teamA --role attacker --x -20 --y -17 &
+PID1=$!
+
+sleep 1
+python3 src/main.py --team teamA --role attacker --x -20 --y 17 &
+PID2=$!
+
+sleep 1
+python3 src/main.py --team teamB --role defender --x -5 --y -5 &
+PID3=$!
+
+sleep 1
+python3 src/main.py --team teamB --role defender --x -5 --y 5 &
+PID4=$!
+
+sleep 1
+python3 src/main.py --team teamB --role goalie --x -50 --y 0 &
+PID5=$!
+
 echo "Нажмите Enter для завершения"
 read
-kill %1 2>/dev/null
+
+kill $PID1 $PID2 $PID3 $PID4 $PID5 2>/dev/null
