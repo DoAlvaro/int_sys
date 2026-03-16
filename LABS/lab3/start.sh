7c26ff50051b0967e44f0ef3deb79fa4e06bf57a
DoAlvaro <alvaro.dobro@mail.ru>
2026-03-16 14:13:44 +0300

    lab3?

--- LABS/lab3/start.sh
+++ LABS/lab3/start.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-python3 src/main.py --team teamA --x -15 --y 0 &
+
+ACTIONS='[{"act": "flag", "fl": "frb"}, {"act": "kick", "fl": "b", "goal": "gr"}]'
+
+python3 src/main.py --team teamA --x 0 --y 0 --actions "$ACTIONS" &
+PID1=$!
+
+sleep 1
+python3 src/main.py --team teamA --x 3 --y 0 --actions "$ACTIONS" &
+PID2=$!
+
+sleep 1
+python3 src/main.py --team teamA --x 0 --y 3 --actions "$ACTIONS" &
+PID3=$!
+
+sleep 1
+python3 src/main.py --team teamB --x -45 --y 0 --goalie &
+PID4=$!
+
 echo "Нажмите Enter для завершения"
 read
-kill %1 2>/dev/null
+
+kill $PID1 $PID2 $PID3 $PID4 2>/dev/null
