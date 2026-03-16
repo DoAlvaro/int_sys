6337dd8422af2d9af9d737013d4dfb4b7e79e15c
DoAlvaro <alvaro.dobro@mail.ru>
2026-03-16 14:28:53 +0300

    lab4?

--- LABS/lab5/start.sh
+++ LABS/lab5/start.sh
@@ -3,21 +3,8 @@
 python3 src/main.py --team teamA --role attacker --x -20 --y -17 &
 PID1=$!
 
-sleep 1
-python3 src/main.py --team teamA --role attacker --x -20 --y 17 &
-PID2=$!
-
-sleep 1
 python3 src/main.py --team teamB --role defender --x -5 --y -5 &
 PID3=$!
-
-sleep 1
-python3 src/main.py --team teamB --role defender --x -5 --y 5 &
-PID4=$!
-
-sleep 1
-python3 src/main.py --team teamB --role goalie --x -50 --y 0 &
-PID5=$!
 
 echo "Нажмите Enter для завершения"
 read
